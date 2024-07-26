e419f0a61d21c67ed4d97da865b045b29f83733f
Gary Mc Nulty <garymcn17@gmail.com>
2024-07-26 21:45:05 +0530

    Basic go backend server

--- backendServer.go
+++ backendServer.go
@@ -1,16 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type User struct {
+	ID	string `json:"id"`
+	Username 	string `json:"username"`
+	Email string `json:"email"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Hello, world!")
+var users []User
+
+
+
+func main() {
+	users = append(users, User{ID: "1", Username: "Gary Tester", Email: "TesterGary@gmail.com"})
+	users = append(users, User{ID: "2", Username: "John Smalls", Email: "Jsmalls@gmail.com"})
+
+
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/createUser", createUsers).Methods("POST")
+	//http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+func getUsers(w http.ResponseWriter, r *http.Request){
+	json.NewEncoder(w).Encode(users)
+}
+
+func createUsers(w http.ResponseWriter, r *http.Request){
+	var newUser User
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	users = append(users, newUser)
+	json.NewEncoder(w).Encode(newUser)
+}
+
+// func handler(w http.ResponseWriter, r *http.Request){
+// 	fmt.Fprint(w, "Hello, world!")
+// }
